2a4748c9216fb58de297d42c8f3bc3169815692f
Fatemehesk <fatemeh.es6717@gmail.com>
2023-02-24 14:00:40 +0200

    Adding Header part of the app part-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import "./Style/header.scss";
 import HeaderData from "./Components/HeaderData";
+
 function App() {
   return (
-    <div className="App">
+    <div className="App ">
       <HeaderData />
     </div>
   );
