0c7ef0b12a36c3874a21896e6f1c9a03281df71c
Fatemehesk <fatemeh.es6717@gmail.com>
2023-02-24 21:35:44 +0200

    Adding day cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import "./Style/header.scss";
 import HeaderData from "./Components/HeaderData";
+import DayCards from "./Components/DayCards";
 
 function App() {
   return (
     <div className="App ">
       <HeaderData />
+      <DayCards />
     </div>
   );
 }
