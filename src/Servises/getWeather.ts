9bff9f0bb07571fdea37ec05ab897c8c4f967de6
Fatemehesk <fatemeh.es6717@gmail.com>
2023-02-27 21:39:14 +0200

    updating get weather function

--- src/Servises/getWeather.ts
+++ src/Servises/getWeather.ts
@@ -21,30 +21,35 @@ export function getWeather(lat: number, lon: number, timezone: string) {
     });
 }
 
-const parseCurrentWeather = (data: {}) => {
-  //   const {
-  //     // temperature: currentTemp,
-  //     // windSpeed: windSpeed,
-  //     // weathercode: iconCode,
-  //   } = data.current_weather;
+const parseCurrentWeather = (arg: { current_weather: any; daily: any }) => {
+  interface currentWeather {
+    temperature: number;
+    windSpeed: number;
+    weathercode: string;
+  }
+  const {
+    temperature: currentTemp,
+    windSpeed: windSpeed,
+    weathercode: iconCode,
+  } = arg.current_weather;
 
-  //   const {
-  // temperature_2m_max: [maxTemp],
-  // temperature_2m_min: [minTemp],
-  // apparent_temperature_max: [maxFeelsLike],
-  // apparent_temperature_min: [minFeelsLike],
-  // precipitation_sum: [precip],
-  //   } = data.daily;
+  const {
+    temperature_2m_max: [maxTemp],
+    temperature_2m_min: [minTemp],
+    apparent_temperature_max: [maxFeelsLike],
+    apparent_temperature_min: [minFeelsLike],
+    precipitation_sum: [precip],
+  } = arg.daily;
 
   return {
-    // currentTemp,
-    // highTemp: maxTemp,
-    // lowTemp: minTemp,
-    // highfeelsTemp: maxFeelsLike,
-    // lowfeelsTemp: minFeelsLike,
-    // windSpeed,
-    // precip: precip,
-    // iconCode,
+    currentTemp,
+    highTemp: maxTemp,
+    lowTemp: minTemp,
+    highfeelsTemp: maxFeelsLike,
+    lowfeelsTemp: minFeelsLike,
+    windSpeed,
+    precip: precip,
+    iconCode,
   };
 };
 const parseDailyWeather = (data: {}) => {};
