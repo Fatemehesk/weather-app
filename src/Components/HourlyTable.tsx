c89c1694e112b53f8f6b2cdc0b55e8590113e555
Fatemehesk <fatemeh.es6717@gmail.com>
2023-03-19 22:46:20 +0200

    fix mapping hourly data Fixes

--- src/Components/HourlyTable.tsx
+++ src/Components/HourlyTable.tsx
@@ -26,88 +26,76 @@ const HourlyTable: FC = (): JSX.Element => {
       sethourlyTemp(res.hourly);
     });
   }, []);
-  console.log(hourlyTemp, "hourly");
+  let rows;
+  if (hourlyTemp) {
+    rows = hourlyTemp!.time.map((time, index) => {
+      return (
+        <div key={time} className="my-3 border">
+          <div className="mx-5 px-5">{dateFormatterFunc(time)}</div>
+          <span className="mx-5 px-5">{dateFormatterToHourFunc(time)}</span>
+          <span className="mx-5 px-5">
+            <Image src={cloud} width={70} />
+          </span>
+          <span className="mx-5 px-5">
+            {hourlyTemp!.apparent_temperature[index]} &deg;
+          </span>
+          <span className="mx-5 px-5">
+            {hourlyTemp!.temperature_2m[index]}&deg;
+          </span>
+          <span className="mx-5 px-5">
+            {hourlyTemp!.windspeed_10m[index]} Mph
+          </span>
+          <span className="mx-5 px-5">
+            {hourlyTemp!.precipitation_probability[index]} in
+          </span>
+        </div>
+      );
+    });
+  }
+
   return (
     <Row className="mt-4 px-2 overflow-hidden">
-      {hourlyTemp && hourlyTemp
-        ? Array.from(Array(Object.values(hourlyTemp).length), (_, index) => {
-            console.log(index);
-            return (
-              <Col
-                xs={12}
-                className="d-flex  align-items-center justify-content-around hourly-section overflow-hidden"
-              >
-                <div className=" d-flex flex-column mb-3 text-uppercase">
-                  <div className="my-3">
-                    {dateFormatterFunc(hourlyTemp.time[index])}
-                  </div>
-                  <span>{dateFormatterToHourFunc(hourlyTemp.time[index])}</span>
-                </div>
-                <Image src={cloud} width={70} />
-                <div className="">
-                  <div className="my-3">Temp</div>
-                  <span data-current-temp>
-                    {hourlyTemp.apparent_temperature[index]} &deg;
-                  </span>
-                </div>
-                <div className="">
-                  <div className="my-3">FL Temp</div>
-                  <span data-current-temp>
-                    {hourlyTemp.temperature_2m[index]}&deg;
-                  </span>
-                </div>
-                <div className="">
-                  <div className="my-3">Wind</div>
-                  <span data-current-temp>
-                    {hourlyTemp.windspeed_10m[index]} Mph
-                  </span>
-                </div>
-                <div className=" ">
-                  <div className="my-3">Precip</div>
-                  <span data-current-precip>
-                    {hourlyTemp.precipitation_probability[index]} in
-                  </span>
-                </div>
-              </Col>
-            );
-          })
-        : ""}
+      <Col
+        xs={12}
+        className="d-flex  align-items-center justify-content-around hourly-section overflow-hidden"
+      >
+        <div className=" d-flex flex-column mb-3 text-uppercase">
+          <div className=" d-flex flex-row align-items-center justify-content-around border py-3 mt-3 mx-5 px-5">
+            <div>Date</div>
+
+            <div>Weadiver</div>
+            <div>Temp</div>
+            <div>FL Temp</div>
+            <div>Wind</div>
+            <div>Precip</div>
+          </div>
+          <div>{rows}</div>
+        </div>
+      </Col>
     </Row>
   );
 };
 
 export default HourlyTable;
 
-// const data = {
-//   fruits: ["apple", "banana", "orange"],
-//   vegetables: ["carrot", "broccoli", "spinach"],
-//   drinks: ["water", "juice", "soda"],
+//   return (
+//     <Row className="mt-4 px-2 overflow-hidden">
+//       <table>
+//         <thead>
+//           <tr>
+//             <th>Date</th>
+//             <th>Time</th>
+//             <th>Weather</th>
+//             <th>Temp</th>
+//             <th>FL Temp</th>
+//             <th>Wind</th>
+//             <th>Precip</th>
+//           </tr>
+//         </thead>
+//         <tbody>{rows}</tbody>
+//       </table>
+//     </Row>
+//   );
 // };
 
-// const combinedData = Object.values(data).reduce(
-//   (acc, curr) => acc.map((el, i) => [...el, curr[i]]),
-//   [[]]
-// );
-
-// return (
-//   <div>
-//     <table>
-//       <thead>
-//         <tr>
-//           {Object.keys(data).map((key) => (
-//             <th key={key}>{key}</th>
-//           ))}
-//         </tr>
-//       </thead>
-//       <tbody>
-//         {combinedData.map((row, i) => (
-//           <tr key={i}>
-//             {row.map((cell, j) => (
-//               <td key={j}>{cell}</td>
-//             ))}
-//           </tr>
-//         ))}
-//       </tbody>
-//     </table>
-//   </div>
-// );
+// export default HourlyTable;
